29c0b6a6e0982b65f581e57d8f8dba15d88f07c8
glrssun <glrssun@gmx.com>
2017-12-05 18:46:26 +0700

    Reupload

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ io.on('connection', function (socket) {
     require('./account')(socket);
 });
 
+app.get('/', function (req, res) {
+    res.render('index');
+});
+
 http.listen(port, function () {
     console.log('listening on *:'+port);
 });
