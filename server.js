5d3448511fe38a33537a24731580e1fa0b77d59a
glrssun <glrssun@gmx.com>
2017-12-07 01:02:24 +0700

    Update

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
+app.get('/', function(req, res){
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    res.end("Server running !! \n");
+});
 
 io.on('connection', function (socket) {
     console.log('user ' + socket.id + ' connected');
