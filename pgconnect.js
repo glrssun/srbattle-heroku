390aab1b4f9ea454a8f258878faeeff73e3b2627
glrssun <glrssun@gmx.com>
2017-12-07 22:33:45 +0700

    Update

--- pgconnect.js
+++ pgconnect.js
@@ -1,6 +1,6 @@
 var pg = require('pg');
 
-var conString = process.env.ELEPHANTSQL_URL || "postgres://postgres:zxcvbnm@localhost:5432/srbattle";
+var conString = "postgres://kyamrjbjyajcde:ae6032eec7570b55df6d0853fe05f1d5b525c4c8a779459af40da2bb460a3e40@ec2-46-137-94-97.eu-west-1.compute.amazonaws.com:5432/dch3n74b9gp6o2"  || "postgres://postgres:zxcvbnm@localhost:5432/srbattle";
 
 var connection = new pg.Client(conString);
 
