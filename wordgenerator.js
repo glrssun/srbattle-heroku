875dd8497aa0a39aaafde3f57cb0c55e9d6457fc
glrssun <glrssun@gmx.com>
2017-12-11 00:00:51 +0700

    Update

--- wordgenerator.js
+++ wordgenerator.js
@@ -30,7 +30,7 @@ exports.createGrid = function (gridSize, words) {
     return grid;
 };
 
-var placeword = function (gridSize, word, grid) {
+function placeword(gridSize, word, grid) {
     var x = Math.floor(Math.random() * gridSize);
     var y = Math.floor(Math.random() * gridSize);
 
@@ -40,33 +40,35 @@ var placeword = function (gridSize, word, grid) {
     word.split('');
     orientation = orientations[Math.floor(Math.random() * orientations.length)];
 
-    console.log('Position y is '+x+' and x is'+y+' while the orientation is +'+orientation);
+    if (word.length !== 0){
+        console.log('Position y is '+x+' and x is'+y+' while the orientation is +'+orientation);
 
-    if (orientation === 'horizontal' && !( y + word.length > gridSize)) {
-        for (var i = 0; i < word.length; i++) {
-            if (grid[x][y + i] === '' || grid[x][y + i] === word[i]) {
-                grid[x][y + i] = word[i];
-            } else placeword(gridSize, word, grid);
+        if (orientation === 'horizontal' && !( y + word.length > gridSize)) {
+            for (var i = 0; i < word.length; i++) {
+                if (grid[x][y + i] === '' || grid[x][y + i] === word[i]) {
+                    grid[x][y + i] = word[i];
+                } else placeword(gridSize, word, grid);
+            }
+        } else if (orientation === 'vertical' && !( x + word.length > gridSize)) {
+            for (i = 0; i < word.length; i++) {
+                if (grid[x + i][y] === '' || grid[x + i][y] === word[i]) {
+                    grid[x + i][y] = word[i];
+                } else placeword(gridSize, word, grid);
+            }
+        } else if (orientation === 'diagonal' && !((x + word.length > gridSize) || (y + word.length > gridSize))) {
+            for (i = 0; i < word.length; i++) {
+                if (grid[x + i][y + i] === '' || grid[x + i][y + i] === word[i]) {
+                    grid[x + i][y + i] = word[i];
+                } else placeword(gridSize, word, grid);
+            }
+        } else if (orientation === 'diagonalUp' && !((x - word.length < 0) || (y + word.length > gridSize))) {
+            for (i = 0; i < word.length; i++) {
+                if (grid[x - i][y + i] === '' || grid[x - i][y + i] === word[i]) {
+                    grid[x - i][y + i] = word[i];
+                } else placeword(gridSize, word, grid);
+            }
+        }else{
+            placeword(gridSize, word, grid);
         }
-    } else if (orientation === 'vertical' && !( x + word.length > gridSize)) {
-        for (i = 0; i < word.length; i++) {
-            if (grid[x + i][y] === '' || grid[x + i][y] === word[i]) {
-                grid[x + i][y] = word[i];
-            } else placeword(gridSize, word, grid);
-        }
-    } else if (orientation === 'diagonal' && !((x + word.length > gridSize) || (y + word.length > gridSize))) {
-        for (i = 0; i < word.length; i++) {
-            if (grid[x + i][y + i] === '' || grid[x + i][y + i] === word[i]) {
-                grid[x + i][y + i] = word[i];
-            } else placeword(gridSize, word, grid);
-        }
-    } else if (orientation === 'diagonalUp' && !((x - word.length < 0) || (y + word.length > gridSize))) {
-        for (i = 0; i < word.length; i++) {
-            if (grid[x - i][y + i] === '' || grid[x - i][y + i] === word[i]) {
-                grid[x - i][y + i] = word[i];
-            } else placeword(gridSize, word, grid);
-        }
-    }else{
-        placeword(gridSize, word, grid);
     }
 };
