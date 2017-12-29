213e169007cfb20e1c96404068225303f4d801f1
glrssun <glrssun@gmx.com>
2017-12-29 20:51:25 +0700

    update

--- mongo_connect.js
+++ mongo_connect.js
@@ -1,5 +1,5 @@
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/srbattle" || "mongodb://glrssun:zxcvbnm@ds133077.mlab.com:33077/srbattle";
+var url = ENV['MONGODB_URI'] || "mongodb://glrssun:zxcvbnm@ds133077.mlab.com:33077/srbattle";
 var mongodb = null;
 
 MongoClient.connect(url, function (err, db) {
