ad052fb2fed24be91c1fc23d0b75df6052eeace5
glrssun <glrssun@gmx.com>
2017-12-29 21:14:30 +0700

    update

--- mongo_connect.js
+++ mongo_connect.js
@@ -5,9 +5,10 @@ var mongodb = null;
 MongoClient.connect(url, function (err, db) {
     if(!err) {
         console.log("Database is connected ... nn");
+        mongodb = db;
     } else {
         console.log("Error connecting database ... nn"+err);
-        mongodb = db;
+
     }
 });
 
