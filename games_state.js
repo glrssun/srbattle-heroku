0fb1410d165a3160ab9963c12688f30a89365d5f
glrssun <glrssun@gmx.com>
2017-12-30 21:14:36 +0700

    update

--- games_state.js
+++ games_state.js
@@ -10,7 +10,7 @@ var host = [];
 
 module.exports = function (socket, io) {
     socket.on('request game', function (data) {
-        mongodb.collection("game_material").findOne(function (err, res) {
+        mongodb.collection("game_material").findOne().toArray(function (err, res) {
             if(!err){
                 console.log('Answer number one = '+res[0].answer1);
                 var grid = gen.createGrid(11, [res[0].answer1, res[0].answer2, res[0].answer3]);
